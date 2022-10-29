06562e07df5a73749ce3283c49a37ea871497798
Luciakestel <luciakestelpires@gmail.com>
2022-10-29 19:28:57 +0200

    add js to form

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,25 +1,80 @@
-const arrow = document.querySelector('.legend--arrow'); //flecha para abrir y cerrar los menús
-const designBox= document.querySelector('.js-design'); //div de "diseña"
-const fillBox= document.querySelector('.js-fill'); //div de "rellena"
-const formBox= document.querySelector('.js-form'); //div de "comparte"
+const arrowUp = document.querySelectorAll('.legend--arrow--up'); //flecha para cerrar los menús
+const arrowDown = document.querySelectorAll('.legend--arrow--down'); //flecha para abrir los menús
+const designBox = document.querySelector('.js-design'); //div de "diseña"
+const fillBox = document.querySelector('.js-fill'); //div de "rellena"
+const formBox = document.querySelector('.js-form'); //div de "comparte"
 
 
 
 "use strict";
-arrow.addEventListener('click', (event) =>{
+//funciones para "diseña"
+
+function classCollapseDesign (event) {
     event.preventDefault();
-    console.log(event.currentTarget);
-    if (event.currentTarget.classList.contains('diseña')){        
-        fillBox.classList.add('collapse');
-        formBox.classList.add('collapse');
-        designBox.classList.toggle('collapse');
-    }  else if  (event.currentTarget.classList.contains('comparte')){ 
-        console.log (event.currentTarget);
-        fillBox.classList.add('collapse');
-        designBox.classList.add('collapse');
-        formBox.classList.remove('collapse');
+    arrowDesign();
+    fillBox.classList.add('collapse');
+    formBox.classList.add('collapse');
+    designBox.classList.toggle('collapse');
+}
+function arrowDesign (event){
+    if (arrowUp[0].classList.contains('collapse')){
+        arrowUp[0].classList.remove('collapse');
+        arrowDown[0].classList.add('collapse');
+    }else {
+        arrowUp[0].classList.add('collapse');
+        arrowDown[0].classList.remove('collapse');
     }
-});
-//flechita para abajo <i class="fa-solid fa-angle-down"></i>
-// flechita para arriba <i class="fa-solid fa-angle-up"></i>
+}
+
+//funciones para "rellena"
+
+function classCollapseFill (event) {
+    event.preventDefault();
+    arrowFill();
+    formBox.classList.add('collapse');
+    designBox.classList.add('collapse');
+    fillBox.classList.toggle('collapse');
+}
+
+function arrowFill (event){
+    if (arrowUp[1].classList.contains('collapse')){
+        arrowUp[1].classList.remove('collapse');
+        arrowDown[1].classList.add('collapse');
+    }else {
+        arrowUp[1].classList.add('collapse');
+        arrowDown[1].classList.remove('collapse');
+    }
+}
+
+//funciones para "comparte"
+function classCollapseForm (event) {
+    event.preventDefault();  
+    arrowForm ();        
+    designBox.classList.add('collapse');
+    fillBox.classList.add('collapse');   
+    formBox.classList.toggle('collapse');
+};
+
+function arrowForm (event){
+    if (arrowUp[2].classList.contains('collapse')){
+        arrowUp[2].classList.remove('collapse');
+        arrowDown[2].classList.add('collapse');
+    }else {
+        arrowUp[2].classList.add('collapse');
+        arrowDown[2].classList.remove('collapse');
+    }
+};
+
+//eventos  
+arrowUp[0].addEventListener('click', classCollapseDesign);
+arrowUp[1].addEventListener('click', classCollapseFill);
+arrowUp[2].addEventListener('click', classCollapseForm);
+
+arrowDown[0].addEventListener('click', classCollapseDesign);
+arrowDown[1].addEventListener('click', classCollapseFill);
+arrowDown[2].addEventListener('click', classCollapseForm);
+
+
+"use strict";
+
 //# sourceMappingURL=main.js.map
--- src/js/01-variables.js
+++ src/js/01-variables.js
@@ -1,5 +1,6 @@
-const arrow = document.querySelector('.legend--arrow'); //flecha para abrir y cerrar los menús
-const designBox= document.querySelector('.js-design'); //div de "diseña"
-const fillBox= document.querySelector('.js-fill'); //div de "rellena"
-const formBox= document.querySelector('.js-form'); //div de "comparte"
+const arrowUp = document.querySelectorAll('.legend--arrow--up'); //flecha para cerrar los menús
+const arrowDown = document.querySelectorAll('.legend--arrow--down'); //flecha para abrir los menús
+const designBox = document.querySelector('.js-design'); //div de "diseña"
+const fillBox = document.querySelector('.js-fill'); //div de "rellena"
+const formBox = document.querySelector('.js-form'); //div de "comparte"
 
